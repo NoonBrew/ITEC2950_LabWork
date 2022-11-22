2dfc14486126cc98c965e4be047cf6bd500ecabc
Nate Owens <n.owens.dev01@gmail.com>
2022-11-21 19:44:16 -0600

    finished the first two videos for the video collector app

--- WeekTwelve/video_collector/video/video_collection/views.py
+++ WeekTwelve/video_collector/video/video_collection/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import VideoForm
 
-# Create your views here.
+
+def home(request):
+    app_name = 'Intreasts Videos'
+    return render(request, 'video_collection/home.html', {'app_name': app_name})
+
+def add(request):
+
+    if request.method == 'POST':
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            new_video_form.save()
+            messages.info(request, 'New Video Saved!')
+            # todo give user input (success message or redirct)
+        else:
+            messages.warning(request, 'Please check the data entered.')
+            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+    new_video_form = VideoForm()
+
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
