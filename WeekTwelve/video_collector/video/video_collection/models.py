51667496f4c5f0874d5d900102600b2a32ac7f59
Nate Owens <n.owens.dev01@gmail.com>
2022-11-22 19:16:20 -0600

    Two videos left for the video collector app

--- WeekTwelve/video_collector/video/video_collection/models.py
+++ WeekTwelve/video_collector/video/video_collection/models.py
@@ -1,9 +1,30 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from urllib import parse
+
 
 class Video(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=400)
     notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True)
+
+    def save(self, *args, **kwargs):
+        # Extract the video id from a youtube url and raise exceptions if it is not a valid url
+        if not self.url.startswith('https://www.youtube.com/watch'):
+            raise ValidationError(f'Not a YouTube URL {self.url}')
+
+        url_components = parse.urlparse(self.url)
+        query_string = url_components.query # 'v=asbakd2312'
+        if not query_string:
+            raise ValidationError(f'Invalid YouTube URL {self.url}')
+        parameters = parse.parse_qs(query_string, strict_parsing=True) # Dictionary
+        v_parameters_list = parameters.get('v') # Returns None if no key found, e.g. abc=1234
+        if not v_parameters_list: # Check if None or empty list
+            raise ValidationError(f'Invalid YouTube URL, missing parameters {self.url}')
+        self.video_id = v_parameters_list[0] # String
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Video ID: {self.video_id}, Notes: {self.notes[:200]}'
--- WeekTwelve/video_collector/video/video_collection/views.py
+++ WeekTwelve/video_collector/video/video_collection/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from .forms import VideoForm
+from .models import Video
 
 
 def home(request):
@@ -12,13 +15,23 @@ def add(request):
     if request.method == 'POST':
         new_video_form = VideoForm(request.POST)
         if new_video_form.is_valid():
-            new_video_form.save()
-            messages.info(request, 'New Video Saved!')
+            try:
+                new_video_form.save()
+                return redirect('video_list')
+                # messages.info(request, 'New Video Saved!')
             # todo give user input (success message or redirct)
-        else:
-            messages.warning(request, 'Please check the data entered.')
-            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+            except ValidationError:
+                messages.warning(request, 'Invalid YouTube URL')
+            except IntegrityError:
+                messages.warning(request, 'You have already added that video.')
+        
+        messages.warning(request, 'Please check the data entered.')
+        return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
 
     new_video_form = VideoForm()
 
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+def video_list(request):
+    videos = Video.objects.all()
+    return render(request, 'video_collection/video_list.html', {'videos': videos})
